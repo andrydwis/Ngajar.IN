8e841ec8c9c6debe9481fafd87da568cb3a2cb6f
Salawat Dedaunan <andry.dwi.s@gmail.com>
2020-11-22 16:07:43 +0700

    add chart reqeust

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,15 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use Carbon\Carbon;
+
 class DashboardController extends Controller
 {
     //
     public function index()
     {
-        if(auth()->user()->status == 'unverified'){
+        $year = Carbon::now()->format('Y');
+        $results = Order::selectRaw('year(created_at) year, monthname(created_at) month, count(*) data')
+            ->groupBy('year', 'month')
+            ->orderBy('year', 'desc')
+            ->get()->where('year', $year);
+        $data = [
+            'year' => $year,
+            'results' => $results
+        ];
+        if (auth()->user()->status == 'unverified') {
             session()->flash('status', 'Your account is still not verified by admin, Please wait');
         }
-        return view('dashboard.index');
+        return view('dashboard.index', $data);
     }
 
     public function profile()
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,30 +1,64 @@
 @extends('layout.app')
 @section('content')
 @include('layout.alert')
-        @guest         
-        @include('layout.front')
-        @include('auth.login')
-        
-        @include('auth.register')
-        @endguest
-          
-        @auth
-        @if(Auth::user()->role == 'client')
-        @include('layout.frontclientmentor')
-        @endif
+@guest
+@include('layout.front')
+@include('auth.login')
 
-        @if(Auth::user()->role == 'mentor')        
-        @include('layout.frontclientmentor')
-        @endif
+@include('auth.register')
+@endguest
 
-        @if(Auth::user()->role == 'admin')
-        
-        @include('layout.frontadmin')
-        @endif
-         
-        @csrf
+@auth
+@if(Auth::user()->role == 'client')
+@include('layout.frontclientmentor')
+@endif
 
-        @endauth
-        @include('dashboard.ourprofile')        
-        @include('dashboard.team')
+@if(Auth::user()->role == 'mentor')
+@include('layout.frontclientmentor')
+@endif
+
+@if(Auth::user()->role == 'admin')
+
+@include('layout.frontadmin')
+<!-- chart -->
+<div class="container p-5">
+        <h1 class="text-center">Data Order Request tahun {{$year}}</h1>
+        <canvas id="myChart" width="400" height="200"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"></script>
+<script>
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+                type: 'doughnut',
+                data: {
+                        labels: [
+                                @foreach($results as $result)
+                                '{{$result->month}}',
+                                @endforeach
+                        ],
+                        datasets: [{
+                                label: 'Jumlah order request',
+                                data: [
+                                        @foreach($results as $result)
+                                        {{$result->data.','}}
+                                        @endforeach
+                                ],
+                                backgroundColor: [
+                                        'rgba(255, 99, 132, 1)',
+                                        'rgba(54, 162, 235, 1)',
+                                        'rgba(255, 206, 86, 1)',
+                                        'rgba(75, 192, 192, 1)',
+                                ]
+
+                        }]
+                },
+        });
+</script>
+@endif
+
+@csrf
+
+@endauth
+@include('dashboard.ourprofile')
+@include('dashboard.team')
 @endsection('content')
